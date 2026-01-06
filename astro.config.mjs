67102dab938688b23ffcd5f35851bf4e09cc91c2
xiaomanzai <1192526749b@gmail.com>
2026-01-07 01:21:01 +0800

    feat: 更新 Astro 配置，添加 Tailwind CSS 插件支持

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://SeasyecN.github.io',
   integrations: [react()],
 
   vite: {
